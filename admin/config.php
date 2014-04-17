6300f2b1bb968112713666ec0bf3512a52d585ba
budiperkasa <budi.perkasa10@gmail.com>
2014-04-17 07:59:08 -0700

    add some value

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/');
+
+// DIR
+define('DIR_APPLICATION', '/vagrant/www/admin/');
+define('DIR_SYSTEM', '/vagrant/www/system/');
+define('DIR_DATABASE', '/vagrant/www/system/database/');
+define('DIR_LANGUAGE', '/vagrant/www/admin/language/');
+define('DIR_TEMPLATE', '/vagrant/www/admin/view/template/');
+define('DIR_CONFIG', '/vagrant/www/system/config/');
+define('DIR_IMAGE', '/vagrant/www/image/');
+define('DIR_CACHE', '/vagrant/www/system/cache/');
+define('DIR_DOWNLOAD', '/vagrant/www/download/');
+define('DIR_LOGS', '/vagrant/www/system/logs/');
+define('DIR_CATALOG', '/vagrant/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/');
+
+// DIR
+define('DIR_APPLICATION', '/vagrant/www/catalog/');
+define('DIR_SYSTEM', '/vagrant/www/system/');
+define('DIR_DATABASE', '/vagrant/www/system/database/');
+define('DIR_LANGUAGE', '/vagrant/www/catalog/language/');
+define('DIR_TEMPLATE', '/vagrant/www/catalog/view/theme/');
+define('DIR_CONFIG', '/vagrant/www/system/config/');
+define('DIR_IMAGE', '/vagrant/www/image/');
+define('DIR_CACHE', '/vagrant/www/system/cache/');
+define('DIR_DOWNLOAD', '/vagrant/www/download/');
+define('DIR_LOGS', '/vagrant/www/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
